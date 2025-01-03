4d0b33b8b8c1d534e89d2ae5505d4bf5f2c9a170
martin76ec <larreaamartin@gmail.com>
2025-01-02 22:24:42 -0500

    fix: apply new line lenght format

--- src/utils/text_spltr.py
+++ src/utils/text_spltr.py
@@ -4,8 +4,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def doc_split(doc: Document):
-    text_splitter: Final = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200, add_start_index=True
-    )
+    text_splitter: Final = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
 
     return text_splitter.split_documents([doc])
